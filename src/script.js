552d9442401f8ec2575d2e01499a9d1989871c18
charlottej2017 <Charlottejones74@gmail.com>
2025-08-22 19:05:35 -0400

    updated api link previous link no longer free

--- src/script.js
+++ src/script.js
@@ -70,7 +70,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   let apiKey = "b95f9ececad46adfb2b7b5be4da60099";
 
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
